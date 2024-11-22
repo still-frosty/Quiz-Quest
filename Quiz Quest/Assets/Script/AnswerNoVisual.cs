010038477a13f6f07f341b0357ea015280bcd6e1
Jemila Ibrahim D. <83028679+Jemeelah1@users.noreply.github.com>
2024-11-22 23:18:16 +0100

    Entertainment Scene

---
+++ Quiz Quest/Assets/Script/AnswerNoVisual.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AnswerNoVisual : MonoBehaviour
+{
+    public GameObject answerAbackBlue; // Blue is waiting
+    public GameObject answerAbackGreen; // Green is correct
+    public GameObject answerAbackRed; // Red is wrong
+
+    public GameObject answerBbackBlue; // Blue is waiting
+    public GameObject answerBbackGreen; // Green is correct
+    public GameObject answerBbackRed; // Red is wrong
+
+    public GameObject answerCbackBlue; // Blue is waiting
+    public GameObject answerCbackGreen; // Green is correct
+    public GameObject answerCbackRed; // Red is wrong
+
+    public GameObject answerDbackBlue; // Blue is waiting
+    public GameObject answerDbackGreen; // Green is correct
+    public GameObject answerDbackRed; // Red is wrong
+
+    public GameObject answerA;
+    public GameObject answerB;
+    public GameObject answerC;
+    public GameObject answerD;
+
+    public AudioSource correctFX;
+    public AudioSource wrongFX;
+
+    public GameObject currentScore;
+    public int scoreValue;
+    public int bestScore;
+    public GameObject bestDisplay;
+
+    void Start()
+    {
+        bestScore = PlayerPrefs.GetInt("BestScoreQuiz", 0);
+        bestDisplay.GetComponent<TMP_Text>().text = "Best: " + bestScore;
+    }
+
+    void Update()
+    {
+        currentScore.GetComponent<TMP_Text>().text = "Score: " + scoreValue;
+    }
+
+    public void AnswerA()
+    {
+        if (QuestionEnt.actualAnswer == "A")
+        {
+            answerAbackGreen.SetActive(true);
+            answerAbackBlue.SetActive(false);
+            correctFX.Play();
+            scoreValue += 5;
+        }
+        else
+        {
+            answerAbackRed.SetActive(true);
+            answerAbackBlue.SetActive(false);
+            wrongFX.Play();
+            scoreValue -= 1;
+        }
+        answerA.GetComponent<Button>().enabled = false;
+        answerB.GetComponent<Button>().enabled = false;
+        answerC.GetComponent<Button>().enabled = false;
+        answerD.GetComponent<Button>().enabled = false;
+        StartCoroutine(NextQuestion());
+    }
+
+    public void AnswerB()
+    {
+        if (QuestionEnt.actualAnswer == "B")
+        {
+            answerBbackGreen.SetActive(true);
+            answerBbackBlue.SetActive(false);
+            correctFX.Play();
+            scoreValue += 5;
+        }
+        else
+        {
+            answerBbackRed.SetActive(true);
+            answerBbackBlue.SetActive(false);
+            wrongFX.Play();
+            scoreValue -= 1;
+        }
+        answerA.GetComponent<Button>().enabled = false;
+        answerB.GetComponent<Button>().enabled = false;
+        answerC.GetComponent<Button>().enabled = false;
+        answerD.GetComponent<Button>().enabled = false;
+        StartCoroutine(NextQuestion());
+    }
+
+    public void AnswerC()
+    {
+        if (QuestionEnt.actualAnswer == "C")
+        {
+            answerCbackGreen.SetActive(true);
+            answerCbackBlue.SetActive(false);
+            correctFX.Play();
+            scoreValue += 5;
+        }
+        else
+        {
+            answerCbackRed.SetActive(true);
+            answerCbackBlue.SetActive(false);
+            wrongFX.Play();
+            scoreValue -= 1;
+        }
+        answerA.GetComponent<Button>().enabled = false;
+        answerB.GetComponent<Button>().enabled = false;
+        answerC.GetComponent<Button>().enabled = false;
+        answerD.GetComponent<Button>().enabled = false;
+        StartCoroutine(NextQuestion());
+    }
+
+    public void AnswerD()
+    {
+        if (QuestionEnt.actualAnswer == "D")
+        {
+            answerDbackGreen.SetActive(true);
+            answerDbackBlue.SetActive(false);
+            correctFX.Play();
+            scoreValue += 5;
+        }
+        else
+        {
+            answerDbackRed.SetActive(true);
+            answerDbackBlue.SetActive(false);
+            wrongFX.Play();
+            scoreValue -= 1;
+        }
+        answerA.GetComponent<Button>().enabled = false;
+        answerB.GetComponent<Button>().enabled = false;
+        answerC.GetComponent<Button>().enabled = false;
+        answerD.GetComponent<Button>().enabled = false;
+        StartCoroutine(NextQuestion());
+    }
+
+    IEnumerator NextQuestion()
+    {
+        if (bestScore < scoreValue)
+        {
+            PlayerPrefs.SetInt("BestScoreQuiz", scoreValue);
+            bestScore = scoreValue;
+            bestDisplay.GetComponent<TMP_Text>().text = "Best: " + scoreValue;
+        }
+        yield return new WaitForSeconds(1.5f);
+
+        answerAbackGreen.SetActive(false);
+        answerBbackGreen.SetActive(false);
+        answerCbackGreen.SetActive(false);
+        answerDbackGreen.SetActive(false);
+
+        answerAbackRed.SetActive(false);
+        answerBbackRed.SetActive(false);
+        answerCbackRed.SetActive(false);
+        answerDbackRed.SetActive(false);
+
+        answerAbackBlue.SetActive(true);
+        answerBbackBlue.SetActive(true);
+        answerCbackBlue.SetActive(true);
+        answerDbackBlue.SetActive(true);
+
+        answerA.GetComponent<Button>().enabled = true;
+        answerB.GetComponent<Button>().enabled = true;
+        answerC.GetComponent<Button>().enabled = true;
+        answerD.GetComponent<Button>().enabled = true;
+
+        QuestionEnt.displayingQuestion = false;
+    }
+}
---
+++ Quiz Quest/Assets/Script/QuestionEnt.cs
@@ -0,0 +1,302 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionEnt : MonoBehaviour
+{
+    public static string actualAnswer;
+    public static bool displayingQuestion = false;
+    public int questionNumber;
+
+    void Update()
+    {
+        if (displayingQuestion == false)
+        {
+            displayingQuestion = true;
+            questionNumber = Random.Range(1, 31);
+            if (questionNumber == 1)
+            {
+                QuestionDisplay.newQuestion = "Brendan Fraser starred in the following movies, except which one?";
+                QuestionDisplay.newA = "A. Titanic";
+                QuestionDisplay.newB = "B. Monkeybone";
+                QuestionDisplay.newC = "C. Encino Man";
+                QuestionDisplay.newD = "D. Mrs. Winterbourne";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 2)
+            {
+                QuestionDisplay.newQuestion = "In Finding Nemo, what was the name of Nemo's mom?";
+                QuestionDisplay.newA = "A. Sandy";
+                QuestionDisplay.newB = "B. Coral";
+                QuestionDisplay.newC = "C. Pearl";
+                QuestionDisplay.newD = "D. Shelly";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 3)
+            {
+                QuestionDisplay.newQuestion = "Which former Star Trek actor directed Three Men and a Baby (1987)?";
+                QuestionDisplay.newA = "A. William Shatner";
+                QuestionDisplay.newB = "B. Leonard Nimoy";
+                QuestionDisplay.newC = "C. George Takei";
+                QuestionDisplay.newD = "D. James Doohan";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 4)
+            {
+                QuestionDisplay.newQuestion = "Which of the following James Bond villains is not affiliated with the SPECTRE organization?";
+                QuestionDisplay.newA = "A. Dr. Julius No";
+                QuestionDisplay.newB = "B. Rosa Klebb";
+                QuestionDisplay.newC = "C. Emilio Largo";
+                QuestionDisplay.newD = "D. Auric Goldfinger";
+                actualAnswer = "D";
+            }
+            else if (questionNumber == 5)
+            {
+                QuestionDisplay.newQuestion = "Which town is the setting for the Disney movie The Love Bug (1968)?";
+                QuestionDisplay.newA = "A. San Francisco";
+                QuestionDisplay.newB = "B. Los Angeles";
+                QuestionDisplay.newC = "C. Sacramento";
+                QuestionDisplay.newD = "D. San Jose";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 6)
+            {
+                QuestionDisplay.newQuestion = "Who won the Grammy for Album of the Year in 2023?";
+                QuestionDisplay.newA = "A. Harry Styles";
+                QuestionDisplay.newB = "B. Beyoncé";
+                QuestionDisplay.newC = "C. Adele";
+                QuestionDisplay.newD = "D. Taylor Swift";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 7)
+            {
+                QuestionDisplay.newQuestion = "Which video game series features characters like Master Chief and Cortana?";
+                QuestionDisplay.newA = "A. Halo";
+                QuestionDisplay.newB = "B. Call of Duty";
+                QuestionDisplay.newC = "C. Destiny";
+                QuestionDisplay.newD = "D. Battlefield";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 8)
+            {
+                QuestionDisplay.newQuestion = "What year was Netflix founded?";
+                QuestionDisplay.newA = "A. 1994";
+                QuestionDisplay.newB = "B. 2001";
+                QuestionDisplay.newC = "C. 1997";
+                QuestionDisplay.newD = "D. 2000";
+                actualAnswer = "C";
+            }
+            else if (questionNumber == 9)
+            {
+                QuestionDisplay.newQuestion = "Who directed the movie 'Inception'?";
+                QuestionDisplay.newA = "A. Steven Spielberg";
+                QuestionDisplay.newB = "B. Christopher Nolan";
+                QuestionDisplay.newC = "C. James Cameron";
+                QuestionDisplay.newD = "D. Quentin Tarantino";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 10)
+            {
+                QuestionDisplay.newQuestion = "What is the name of the AI assistant in the Iron Man movies?";
+                QuestionDisplay.newA = "A. J.A.R.V.I.S.";
+                QuestionDisplay.newB = "B. A.L.F.R.E.D.";
+                QuestionDisplay.newC = "C. S.A.M.";
+                QuestionDisplay.newD = "D. K.A.R.E.N.";
+                actualAnswer = "A";
+            }
+            // else if (questionNumber == 11)
+            // {
+            //     QuestionDisplay.newQuestion = "What country flag is this?";
+            //     QuestionDisplay.newA = "A. China";
+            //     QuestionDisplay.newB = "B. Spain";
+            //     QuestionDisplay.newC = "C. South Sudan";
+            //     QuestionDisplay.newD = "D. Germany";
+            //     visual001.SetActive(true);
+            //     actualAnswer = "B";
+            // }
+            else if (questionNumber == 11)
+            {
+                QuestionDisplay.newQuestion = "Who sang the hit song 'Blinding Lights'?";
+                QuestionDisplay.newA = "A. Drake";
+                QuestionDisplay.newB = "B. The Weeknd";
+                QuestionDisplay.newC = "C. Justin Bieber";
+                QuestionDisplay.newD = "D. Bruno Mars";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 12)
+            {
+                QuestionDisplay.newQuestion = "Which character in 'Friends' is known for saying 'We were on a break!'?";
+                QuestionDisplay.newB = "A. Chandler";
+                QuestionDisplay.newC = "B. Joey";
+                QuestionDisplay.newA = "C. Ross";
+                QuestionDisplay.newD = "D. Rachel";
+                actualAnswer = "C";
+            }
+            else if (questionNumber == 13)
+            {
+                QuestionDisplay.newQuestion = "Which actor played Jack Dawson in the movie Titanic?";
+                QuestionDisplay.newA = "A. Brad Pitt";
+                QuestionDisplay.newC = "B. Matt Damon";
+                QuestionDisplay.newD = "C. Johnny Depp";
+                QuestionDisplay.newB = "D. Leonardo DiCaprio";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 14)
+            {
+                QuestionDisplay.newQuestion = "Which video game features the fictional location of Raccoon City?";
+                QuestionDisplay.newA = "A. Silent Hill";
+                QuestionDisplay.newB = "B. Resident Evil";
+                QuestionDisplay.newC = "C. The Last of Us";
+                QuestionDisplay.newD = "D. Dead Space";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 15)
+            {
+                QuestionDisplay.newQuestion = "Which music artist holds the record for the most Grammy Awards won in a single night?";
+                QuestionDisplay.newA = "A. Adele";
+                QuestionDisplay.newB = "B. Beyoncé";
+                QuestionDisplay.newC = "C. Michael Jackson";
+                QuestionDisplay.newD = "D. Taylor Swift";
+                actualAnswer = "C";
+            }
+            else if (questionNumber == 16)
+            {
+                QuestionDisplay.newQuestion = "Which author wrote the book series *A Song of Ice and Fire*?";
+                QuestionDisplay.newA = "A. George R.R. Martin";
+                QuestionDisplay.newB = "B. J.K. Rowling";
+                QuestionDisplay.newC = "C. J.R.R. Tolkien";
+                QuestionDisplay.newD = "D. Stephen King";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 17)
+            {
+                QuestionDisplay.newQuestion = "What is the name of the wizarding bank in the Harry Potter series?";
+                QuestionDisplay.newA = "A. Ollivanders";
+                QuestionDisplay.newB = "B. Gringotts";
+                QuestionDisplay.newC = "C. Diagon Alley";
+                QuestionDisplay.newD = "D. Hogsmeade Bank";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 18)
+            {
+                QuestionDisplay.newQuestion = "Which fictional language is spoken by the Elves in *The Lord of the Rings*?";
+                QuestionDisplay.newA = "A. Klingon";
+                QuestionDisplay.newB = "B. Sindarin";
+                QuestionDisplay.newC = "C. Valyrian";
+                QuestionDisplay.newD = "D. Dothraki";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 19)
+            {
+                QuestionDisplay.newQuestion = "What is the name of the ship in the movie *Pirates of the Caribbean*?";
+                QuestionDisplay.newA = "A. The Jolly Roger";
+                QuestionDisplay.newB = "B. The Flying Dutchman";
+                QuestionDisplay.newC = "C. The Black Pearl";
+                QuestionDisplay.newD = "D. The Queen Anne’s Revenge";
+                actualAnswer = "C";
+            }
+            else if (questionNumber == 20)
+            {
+                QuestionDisplay.newQuestion = "Which classic novel begins with the line 'Call me Ishmael'?";
+                QuestionDisplay.newA = "A. *Moby Dick*";
+                QuestionDisplay.newB = "B. *The Great Gatsby*";
+                QuestionDisplay.newC = "C. *1984*";
+                QuestionDisplay.newD = "D. *Pride and Prejudice*";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 21)
+            {
+                QuestionDisplay.newQuestion = "Which popular TV show is set in the fictional town of Hawkins, Indiana?";
+                QuestionDisplay.newA = "A. The X-Files";
+                QuestionDisplay.newB = "B. Stranger Things";
+                QuestionDisplay.newC = "C. Twin Peaks";
+                QuestionDisplay.newD = "D. Breaking Bad";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 22)
+            {
+                QuestionDisplay.newQuestion = "In which Disney movie does the character Maui appear?";
+                QuestionDisplay.newA = "A. Moana";
+                QuestionDisplay.newB = "B. Frozen";
+                QuestionDisplay.newC = "C. Tangled";
+                QuestionDisplay.newD = "D. Encanto";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 23)
+            {
+                QuestionDisplay.newQuestion = "Which painting is famously housed in the Louvre Museum in Paris?";
+                QuestionDisplay.newA = "A. The Starry Night";
+                QuestionDisplay.newB = "B. Mona Lisa";
+                QuestionDisplay.newC = "C. The Scream";
+                QuestionDisplay.newD = "D. Girl with a Pearl Earring";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 24)
+            {
+                QuestionDisplay.newQuestion = "Which famous musician is known as the 'King of Pop'?";
+                QuestionDisplay.newA = "A. Elvis Presley";
+                QuestionDisplay.newB = "B. Michael Jackson";
+                QuestionDisplay.newC = "C. Prince";
+                QuestionDisplay.newD = "D. Stevie Wonder";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 25)
+            {
+                QuestionDisplay.newQuestion = "What is the name of Beyoncé’s alter ego introduced during her *Sasha Fierce* era?";
+                QuestionDisplay.newA = "A. Queen Bey";
+                QuestionDisplay.newB = "B. Sasha Fierce";
+                QuestionDisplay.newC = "C. Yoncé";
+                QuestionDisplay.newD = "D. Honey Bee";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 26)
+            {
+                QuestionDisplay.newQuestion = "What is the title of the longest-running Broadway musical of all time?";
+                QuestionDisplay.newA = "A. *Les Misérables*";
+                QuestionDisplay.newB = "B. *Cats*";
+                QuestionDisplay.newC = "C. *The Phantom of the Opera*";
+                QuestionDisplay.newD = "D. *Chicago*";
+                actualAnswer = "C";
+            }
+            else if (questionNumber == 27)
+            {
+                QuestionDisplay.newQuestion = "Which K-pop group released the song *Dynamite*?";
+                QuestionDisplay.newA = "A. Blackpink";
+                QuestionDisplay.newB = "B. BTS";
+                QuestionDisplay.newC = "C. EXO";
+                QuestionDisplay.newD = "D. TWICE";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 28)
+            {
+                QuestionDisplay.newQuestion = "Which Netflix series is based on the life of British royalty?";
+                QuestionDisplay.newA = "A. *The Crown*";
+                QuestionDisplay.newB = "B. *Downton Abbey*";
+                QuestionDisplay.newC = "C. *Bridgerton*";
+                QuestionDisplay.newD = "D. *Victoria*";
+                actualAnswer = "A";
+            }
+            else if (questionNumber == 29)
+            {
+                QuestionDisplay.newQuestion = "What is the name of the fictional country in *Black Panther*?";
+                QuestionDisplay.newA = "A. Zamunda";
+                QuestionDisplay.newB = "B. Wakanda";
+                QuestionDisplay.newC = "C. Latveria";
+                QuestionDisplay.newD = "D. Genovia";
+                actualAnswer = "B";
+            }
+            else if (questionNumber == 30)
+            {
+                QuestionDisplay.newQuestion = "Which singer released the album *25* in 2015?";
+                QuestionDisplay.newA = "A. Taylor Swift";
+                QuestionDisplay.newB = "B. Adele";
+                QuestionDisplay.newC = "C. Lady Gaga";
+                QuestionDisplay.newD = "D. Rihanna";
+                actualAnswer = "B";
+            }
+
+            //all question go above this line
+            QuestionDisplay.pleaseUpdate = false;
+        }
+    }
+}
